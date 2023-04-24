6d03607ab66eb65d585aec35137d9753cbaf1eb7
JuanRaV <juanravirgen@gmail.com>
2023-04-23 21:42:44 -0600

    Adding validation in students sign up, hashing passwords and saving students in the database

--- controllers/student.js
+++ controllers/student.js
@@ -1,3 +1,9 @@
+import { PrismaClient } from "@prisma/client"
+import { check, validationResult } from "express-validator"
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
+
 const logInStudent = (req,res)=>{
     res.render('partials/login',{
         type:"Students",
@@ -6,11 +12,85 @@ const logInStudent = (req,res)=>{
 }
 
 const signUpStudent = (req,res)=>{
-    res.render('partials/signup',{
+    res.render('students/signup',{
         type:"Students",
         pagina: 'SignUp'
     })
 }
+
+const register =async (req,res)=>{
+    //Validacion
+    await check('fullName').notEmpty().withMessage('Name is required').run(req)
+    await check('email').isEmail().withMessage("That's not an email").run(req)
+    await check('education').notEmpty().withMessage('Education is required').run(req)
+    await check('password').isLength({min:6}).withMessage('The password must have a minimum of 6 characters').run(req)
+
+
+
+    let result = validationResult(req)
+
+
+    const {fullName,email,education,password,profileImage,cv,link} = req.body
+    //Verifica que el resultado no este vacio
+    if(!result.isEmpty()){
+        return res.render('students/signup',{
+            pagina:'SignUp',
+            type:"Students",
+            errores: result.array(),
+            student:{
+                name:fullName,
+                email,
+                education
+            }
+        })
+    }
+    //Verrificar que el usuario no este registrado
+    
+    const existStudent = await prisma.Student.findUnique({
+        where:{
+            email
+        }
+    })
+
+    if(existStudent){
+        return res.render('students/signup',{
+            pagina:'SignUp',
+            type:"Students",
+            errores: [{msg:'User already registered'}],
+            student:{
+                name:fullName,
+                email,
+                education
+            }
+        })
+    }
+    //Hashing password
+    prisma.$use(async (params, next) => {
+        if (params.model === 'Student' && params.action === 'create') {
+          const password = params.args.data.password;
+          const hashedPassword = await bcrypt.hash(password, 10);
+          params.args.data.password = hashedPassword;
+        }
+      
+        return next(params);
+      });
+      
+    //Almacenar un nuevo usuario 
+    await prisma.Student.create(
+        {data:{
+            fullName,
+            email,
+            password,
+            education,
+            profileImage,
+            cv,
+            link,
+            token:'123'
+        }}
+    ) 
+
+}
+ 
 const formularioOlvidePassword = (req,res)=>{
     res.render('partials/olvide-password',{
         type:"Students",
@@ -20,5 +100,6 @@ const formularioOlvidePassword = (req,res)=>{
 export{
     logInStudent,
     signUpStudent,
+    register,
     formularioOlvidePassword
-}
+} 
