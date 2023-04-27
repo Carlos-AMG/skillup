c1e76be4ef1f920800167b4305812ab5a42afa3a
Carlos Martinez <alberto-cmg@outlook.es>
2023-04-26 21:36:14 -0600

    fix migrations folder

--- prisma/migrations/20230423161418_token_link/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Company" ADD COLUMN     "token" TEXT,
-ADD COLUMN     "verified" BOOLEAN;
-
--- AlterTable
-ALTER TABLE "Student" ADD COLUMN     "link" TEXT,
-ADD COLUMN     "token" TEXT;
--- prisma/migrations/20230427031056_skillup/migration.sql
+++ prisma/migrations/20230427031056_skillup/migration.sql
@@ -5,8 +5,11 @@ CREATE TABLE "Student" (
     "password" TEXT NOT NULL,
     "fullName" TEXT NOT NULL,
     "education" TEXT NOT NULL,
+    "token" TEXT,
     "profileImage" TEXT,
     "cv" TEXT,
+    "link" TEXT,
+    "verified" BOOLEAN,
 
     CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
 );
@@ -20,12 +23,16 @@ CREATE TABLE "Company" (
     "name" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "description" TEXT NOT NULL,
+    "verified" BOOLEAN,
 
     CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Student_email_key" ON "Student"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_token_key" ON "Student"("token");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Company_email_key" ON "Company"("email");
