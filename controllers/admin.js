50164951d923f46d616e4fb54a1a69ad147d765d
Emanuel Vazquez <evazquezrosales@gmail.com>
2023-05-05 14:25:50 -0600

    refactor the admin functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,7 @@
+
 import {getAllCompanies, getAllStudents} from "../helpers/utils.js";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export const getDashboardPage = async (req,res) => {
     const {password} = req.params
@@ -10,5 +13,23 @@ export const getDashboardPage = async (req,res) => {
 
     }else{
         res.render("admin/error")
+    }
+}
+
+//API 
+export const verifyCompany = async (req,res) =>{
+    const {companyId} = req.params
+    try{
+        const verify = await prisma.company.update({
+            where:{
+                id:companyId
+            },
+            data:{
+                verified:true
+            }
+        })
+        res.status(200).json({ message: 'Company verified successfully', verify });
+    }catch{
+        res.status(500).json({ message: 'Error verifying company', error });
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,15 @@
 import express from 'express'
-import{getDashboardPage} from '../controllers/admin.js'
-import { upload } from '../middlewares/upload.js';
+import{getDashboardPage,verifyCompany} from '../controllers/admin.js'
+
 
 const adminRouter = express.Router();
 
 //Render
 
 adminRouter.get("/:password",getDashboardPage);
+
+
+//API
+adminRouter.post("/verify/:companyId",verifyCompany)
 
 export default adminRouter;
