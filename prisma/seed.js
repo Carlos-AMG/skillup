8d9a686cf2db21d2e8746941fc96a10f328cc607
LAPTOP_Efrain <efrain.robles5009@alumnos.udg.mx>
2023-05-19 15:54:24 -0600

    Added 4 students to the seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { isNullOrUndefined } from "url/util";
 const prisma = new PrismaClient();
 
 async function createOrUpdateArea(areaName) {
@@ -12,54 +13,106 @@ async function createOrUpdateArea(areaName) {
 }
 
 async function createCompany(name, email, rfc, address, description,password) {
-  const company = await prisma.company.create({
-    data: {
-      name: name,
-      email: email,
-      rfc: rfc,
-      address: address,
-      description: description,
-      password
-    },
+  const existingEmail = await prisma.company.findUnique({
+    where: {
+      email: email
+    }
   });
+  const existingRFC = await prisma.company.findUnique({
+    where: {
+      rfc: rfc
+    }
+  });
+  if (existingEmail || existingRFC) {
+    console.log('Company with the same email or RFC already exists.');
+  } else {
+    const company = await prisma.company.create({
+      data: {
+        name: name,
+        email: email,
+        rfc: rfc,
+        address: address,
+        description: description,
+        password: password
+      },
+    });
+    return company;
+  }
+}
 
-  return company;
+async function createStudent(email, password, name, education) {
+  const existingStudent = await prisma.student.findUnique({
+    where: {
+      email: email
+    }
+  });
+  if (existingStudent) {
+    console.log('Student with the same email already exists.');
+  } else {
+    const student = await prisma.student.create({
+      data: {
+        email: email,
+        password: password,
+        fullName: name,
+        education: education
+      },
+    });
+    return student;
+  }
 }
 
 async function createJob(title, description, jobType, skills, modality, salary, hoursPerWeek, areaId, companyId) {
-  const job = await prisma.job.create({
-    data: {
-      title: title,
-      description: description,
-      jobType: jobType,
-      skills: skills,
-      modality: modality,
-      salary: salary,
-      hoursPerWeek: hoursPerWeek,
-      areaId: areaId,
-      companyId: companyId,
-
-    },
+  const existingAreaId = await prisma.area.findUnique({
+    where: { area: areaId }
   });
-
-  return job;
+  const existingCompanyId = await prisma.company.findUnique({
+    where: { company: companyId}
+  });
+  if (existingAreaId || existingCompanyId) {
+    const job = await prisma.job.create({
+      data: {
+        title: title,
+        description: description,
+        jobType: jobType,
+        skills: skills,
+        modality: modality,
+        salary: salary,
+        hoursPerWeek: hoursPerWeek,
+        areaId: areaId,
+        companyId: companyId,
+      },
+    });
+    return job;
+  } else {
+    console.log('Job with the area or company doesnt exists.');
+  }
 }
 
 async function createCourse(title, startDate, endDate, description, prerequisites, modality, areaId, companyId) {
-  const course = await prisma.course.create({
-    data: {
-      title: title,
-      startDate: startDate,
-      endDate: endDate,
-      description: description,
-      prerequisites: prerequisites,
-      modality: modality,
-      areaId: areaId,
-      companyId: companyId,
-    },
+  const existingAreaId = await prisma.area.findUnique({
+    where: { area: areaId }
   });
-
-  return course;
+  const existingCompanyId = await prisma.company.findUnique({
+    where: { company: companyId}
+  });
+  if (existingAreaId || existingCompanyId) {
+    console.log(existingAreaId,existingCompanyId)
+    const course = await prisma.course.create({
+      data: {
+        title: title,
+        startDate: startDate,
+        endDate: endDate,
+        description: description,
+        prerequisites: prerequisites,
+        modality: modality,
+        areaId: areaId,
+        companyId: companyId,
+      },
+    });
+    return course;
+  } else {
+    console.log('Course with the area or company doesnt exists.');
+  }
 }
 
 async function main() {
@@ -68,23 +121,42 @@ async function main() {
   const agriculture = await createOrUpdateArea('Agriculture');
   const engineering = await createOrUpdateArea('Engineering');
 
+  const student1 = await createStudent(
+    'mariadelcarmen@gmail.com',
+    'Maria12!',
+    'Maria del Carmen',
+    'Tecmilenio'
+  );
+
+  const student2 = await createStudent(
+    'pablo43delaCruz@hotmail.com',
+    'contraseÑa@90!',
+    'Pablo de la Cruz',
+    'CUCEI'
+  );
+
   const company1 = await createCompany(
     'Google',
     'google@gmail.com',
     'ABCDEFGHIJSA',
     'LOS ANGELES',
     'Empresa enfocada en la navegacion',
-    'es1234567'
+    'Es1234567'
   );
   const company2 = await createCompany(
     'HP',
-    'hp@hp.com',
+    'hp.corp@hp.mx',
     'AAAAA1234',
     'zapa gdl',
     'We make chips',
-    'es123456' 
+    'Es123456' 
   );
 
+  if (company1 === undefined || company2 ===undefined) {
+    console.log('Companies dont exists.');
+    return;
+  }
+  
   const job1 = await createJob(
     ' Jumior Software',
     'Pura programacion',
@@ -94,7 +166,7 @@ async function main() {
     50000,
     40,
     social.id,
-    company1.id
+    company1.id 
   );
   const job2 = await createJob(
     'Senior data bases',
@@ -130,6 +202,7 @@ async function main() {
   );
 
   console.log("Seeded areas: ", social, engineering);
+  console.log("Seeded companies: ", student1, student2);
   console.log("Seeded companies: ", company1,company2);
   console.log("Seeded jobs: ", job1, job2);
   console.log("Seeded courses: ", course1, course2);
